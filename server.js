363a8d2e8b1917231d66650be6e28b133406bc35
Greg Weber <gregoryweberguitars@gmail.com>
2018-08-01 17:49:33 -0700

    got logo path animation working with a svg  gradient fill

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(express.static(__dirname + '/static'));
+
 // if deciding to use body-parser un-comment out next few lines
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
@@ -21,4 +23,4 @@ app.set('views', path.join(__dirname, 'views'));
 require('./server/config/routes')(app);
 
 
-app.listen(port, ()=> console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`listening on port ${port}`));
